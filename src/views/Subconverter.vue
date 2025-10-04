8787bdcf593f1cc19c122f42f2bba54202499099
ikun <218283296+gdydg@users.noreply.github.com>
2025-10-05 00:29:07 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -786,7 +786,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://mdacpfzgucdt.us-west-1.clawcloudrun.com" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://sub4.alina123.ggff.net" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
@@ -1310,4 +1310,5 @@ export default {
   }
 };
 </script>
+
 
